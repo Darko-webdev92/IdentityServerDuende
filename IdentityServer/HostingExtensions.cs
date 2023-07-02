b391e611a85d4689e76889c1c774adbaff4929e1
Darko-webdev92 <37109758+Darko-webdev92@users.noreply.github.com>
2023-07-02 21:27:05 +0200

    Add Asp.Net Identity

--- IdentityServer/HostingExtensions.cs
+++ IdentityServer/HostingExtensions.cs
@@ -1,8 +1,13 @@
 using Duende.IdentityServer.EntityFramework.DbContexts;
 using Duende.IdentityServer.EntityFramework.Mappers;
+using IdentityModel;
+using IdentityServer.Data;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using System.Security.Claims;
 
 namespace IdentityServer;
 
@@ -19,15 +24,30 @@ internal static class HostingExtensions
         // uncomment if you want to add a UI
         builder.Services.AddRazorPages();
 
-        // FOR IN-MEMORY
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                         .AddEntityFrameworkStores<ApplicationDbContext>()
+                         .AddDefaultTokenProviders();
+
+        #region IN-Memmory
         //builder.Services.AddIdentityServer()
         //    .AddInMemoryIdentityResources(Config.IdentityResources)
         //    .AddInMemoryApiScopes(Config.ApiScopes)
         //    .AddInMemoryClients(Config.Clients)
         //    .AddTestUsers(TestUsers.Users);
+        #endregion
 
+        builder.Services.AddIdentityServer(options =>
+                        {
+                            options.Events.RaiseErrorEvents = true;
+                            options.Events.RaiseInformationEvents = true;
+                            options.Events.RaiseFailureEvents = true;
+                            options.Events.RaiseSuccessEvents = true;
 
-        builder.Services.AddIdentityServer()
+                            // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                            options.EmitStaticAudienceClaim = true;
+                        })
                         .AddConfigurationStore(options =>
                         {
 
@@ -39,7 +59,8 @@ internal static class HostingExtensions
                            options.ConfigureDbContext = b => b.UseNpgsql(connectionString,
                                sql => sql.MigrationsAssembly(migrationsAssembly));
                        })
-                        .AddTestUsers(TestUsers.Users);
+                       .AddAspNetIdentity<ApplicationUser>();
+
 
         return builder.Build();
     }
@@ -199,6 +220,79 @@ internal static class HostingExtensions
                 }
             }
             #endregion ApiScopes
+        }
+
+
+        using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        {
+            var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+            context.Database.Migrate();
+
+            var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var alice = userMgr.FindByNameAsync("alice").Result;
+            if (alice == null)
+            {
+                alice = new ApplicationUser
+                {
+                    UserName = "alice",
+                    Email = "AliceSmith@email.com",
+                    EmailConfirmed = true,
+                };
+                var result = userMgr.CreateAsync(alice, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userMgr.AddClaimsAsync(alice, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Alice"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                        }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("alice created");
+            }
+            else
+            {
+                Log.Debug("alice already exists");
+            }
+
+            var bob = userMgr.FindByNameAsync("bob").Result;
+            if (bob == null)
+            {
+                bob = new ApplicationUser
+                {
+                    UserName = "bob",
+                    Email = "BobSmith@email.com",
+                    EmailConfirmed = true
+                };
+                var result = userMgr.CreateAsync(bob, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userMgr.AddClaimsAsync(bob, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Bob"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                            new Claim("location", "somewhere")
+                        }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+                Log.Debug("bob created");
+            }
+            else
+            {
+                Log.Debug("bob already exists");
+            }
         }
     }
 }
